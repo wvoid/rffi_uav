122b08c9ddff3c9997875184d22fa0b9c091ced7
Zhaorui Wang <748339596@qq.com>
2022-10-18 17:44:14 +0800

    10.18 pm

--- get_data_frommat.py
+++ get_data_frommat.py
@@ -6,17 +6,35 @@ import os
 def save_to_traindata():
     # filename = 'F:\matlab\preamble_data\\features_mat_xcorr1'
     # filename1 = 'F:\matlab\preamble_data\\features_mat_xcorr2'
-    filename = '/home/rs/1/preamble_data/features_mat_xcorr1.mat'
-    filename1 = '/home/rs/1/preamble_data/features_mat_xcorr2.mat'
-    data1 = so.loadmat(filename)
-    data2 = so.loadmat(filename1)
-    x1 = data1['features_mat1']
-    x2 = data2['features_mat2']
-    x = np.vstack((x1, x2))
-    y1 = np.zeros((600, 1))
-    y2 = np.ones((600, 1))
-    y = np.vstack((y1, y2))
-    filename2 = 'data_train/data_rf1_rf2_xcorr_test'
+    filename1 = '/home/rs/1/preamble_data/holybro/features_mat_holybro2_524.mat'
+    filename2 = '/home/rs/1/preamble_data/holybro/features_mat_holybro1_590.mat'
+    filename3 = '/home/rs/1/preamble_data/holybro/features_mat_v51_530.mat'
+    filename4 = '/home/rs/1/preamble_data/holybro/features_mat_v52_525.mat'
+
+    data1 = so.loadmat(filename1)
+    data2 = so.loadmat(filename2)
+    data3 = so.loadmat(filename3)
+    data4 = so.loadmat(filename4)
+    x1 = data1['features_mat']
+    x2 = data2['features_mat']
+    x3 = data3['features_mat']
+    x4 = data4['features_mat']
+
+    len_x1 = np.size(x1, 0)
+    len_x2 = np.size(x2, 0)
+    len_x3 = np.size(x3, 0)
+    len_x4 = np.size(x4, 0)
+
+    # print(len_x1)
+    x = np.vstack((x1, x2, x3, x4))
+    y1 = np.zeros((len_x1, 1))
+    y2 = np.ones((len_x2, 1))
+    y3 = np.empty((len_x3, 1))
+    y3[:] = 2
+    y4 = np.empty((len_x4, 1))
+    y4[:] = 3
+    y = np.vstack((y1, y2, y3, y4))
+    filename2 = 'data_train/data_4rfs'
     np.savez(filename2, data=x, label=y)
 
 
--- main.py
+++ main.py
@@ -11,14 +11,14 @@ from matplotlib import pyplot as plt
 from get_data_frommat import save_to_traindata, get_mat
 
 if __name__ == '__main__':
-    save_to_traindata()
-    filename = 'data_train/data_rf1_rf2_ps_test.npz'
+     # save_to_traindata()
+    filename = 'data_train/data_4rfs.npz'
     data = np.load(filename)
     data_pb_ps = data['data'].real
     label = data['label']
 
-    pca = PCA(n_components=5)
-    data_pb_ps = pca.fit_transform(data_pb_ps)
+    # pca = PCA(n_components=5)
+    # data_pb_ps = pca.fit_transform(data_pb_ps)
     # data_pb_ps_x1, data_pb_ps_x2 = np.empty((1, 2)), np.empty((1, 2))
     # for i in range(1200):
     #     if label[i] == 0:
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     x_train, x_test, y_train, y_test = train_test_split(data_pb_ps, label, test_size=0.3)
 
     randomforest = RandomForestClassifier(random_state=42, n_estimators=100)
-    # svm_clf=svm.SVC(kernel='poly',degree=3)
+    # svm_clf=svm.SVC()
 
     randomforest.fit(x_train, y_train.ravel())
     # svm_clf.fit(x_train,y_train.ravel())
@@ -47,6 +47,7 @@ if __name__ == '__main__':
     plot_confusion_matrix(randomforest, x_test, y_test)
     # plot_confusion_matrix(svm_clf,x_test,y_test)
     plt.show()
-    x_pb_test = get_mat('features_mat_test1_rf1')  # .real
-    x_pb_test = pca.transform(x_pb_test)
-    print(randomforest.predict(x_pb_test))
+    # x_pb_test = get_mat('features_mat_test1_holybro1')  # .real
+    # x_pb_test = pca.transform(x_pb_test)
+    # print(randomforest.predict(x_pb_test))
+    # print(svm_clf.predict(x_pb_test))
